c57bb6b358571adf5316eb9f8a5c8e2b0f9ba2b6
sudonona <80674485+sudonona@users.noreply.github.com>
2024-09-21 18:29:08 +0200

    finish

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,11 +2,21 @@ function Footer() {
   const hour = new Date().getHours();
   const openHour = 18;
   const closeHour = 23;
+  const isOpen = hour >= openHour && hour <= closeHour;
   if (hour >= openHour && hour <= closeHour) alert("We,re open");
   else alert("Sorry we're closed");
   return (
     <footer className="footer mt-5 text-center">
-      {new Date().toLocaleTimeString()}. We're currently open
+      {isOpen ? (
+        <div>
+          <p>We're open until {closeHour} :00. Come and visit us </p>
+        </div>
+      ) : (
+        <p>
+          Sorry we're closed. Come and visit us between {openHour}:00 and{" "}
+          {closeHour}:00{" "}
+        </p>
+      )}
     </footer>
   );
 }
--- src/Components/Pizzas.jsx
+++ src/Components/Pizzas.jsx
@@ -1,73 +1,37 @@
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import React from "react";
 
-const pizzaData = [
-  {
-    name: "Focaccia",
-    ingredients: "Bread with italian olive oil and rosemary",
-    price: 6,
-    photoName: "pizzas/focaccia.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Margherita",
-    ingredients: "Tomato and mozarella",
-    price: 10,
-    photoName: "pizzas/margherita.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Spinaci",
-    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-    price: 12,
-    photoName: "pizzas/spinaci.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Funghi",
-    ingredients: "Tomato, mozarella, mushrooms, and onion",
-    price: 12,
-    photoName: "pizzas/funghi.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Salamino",
-    ingredients: "Tomato, mozarella, and pepperoni",
-    price: 15,
-    photoName: "pizzas/salamino.jpg",
-    soldOut: true,
-  },
-  {
-    name: "Pizza Prosciutto",
-    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-    price: 18,
-    photoName: "pizzas/prosciutto.jpg",
-    soldOut: false,
-  },
-];
-
 function Pizza(props) {
   return (
-    <Container>
-      <Row className="d-flex align-items-center">
-        <Col>
-          <Card style={{ width: "12rem" }}>
-            <Card.Img variant="top" src={props.photoName} alt={props.name} />
-          </Card>
-        </Col>
-        <Col>
-          <div>
-            <Card.Body>
-              <Card.Title>
-                <h3>{props.name}</h3>
-              </Card.Title>
-              <Card.Text>{props.ingredients}</Card.Text>
-              <Button variant="primary">{props.price} $</Button>
-            </Card.Body>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <li className={`${props.pizzaObj.soldOut ? "sold-out" : ""} `}>
+      <Container className="d-flex align-items-start">
+        <Row className="d-flex align-items-center justify-content-around ">
+          <Col>
+            <Card style={{ width: "12rem" }}>
+              <Card.Img
+                variant="top"
+                src={props.pizzaObj.photoName}
+                alt={props.pizzaObj.name}
+              />
+            </Card>
+          </Col>
+          <Col>
+            <div>
+              <Card.Body>
+                <Card.Title>
+                  <h3>{props.pizzaObj.name}</h3>
+                </Card.Title>
+                <Card.Text>{props.pizzaObj.ingredients}</Card.Text>
+                <Button variant="primary">
+                  {props.pizzaObj.soldOut ? " SOLD OUT" : props.pizzaObj.price}{" "}
+                  $
+                </Button>
+              </Card.Body>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </li>
   );
 }
 
